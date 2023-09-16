d75d41dee9258cc749474a3d47a88ca30874911e
iy <iy.expert@gmail.com>
2023-09-17 02:58:48 +0300

    Add test to test ping/pingDB

---
+++ tests/SiteLoggerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Support\Facades\DB;
+
+class SiteLoggerTest extends TestCase
+{
+    /**
+     * Ping SiteLogger Microservice Test
+     *
+     * @return void
+     */
+    public function testPing(): void
+    {
+        $this->get('/api/v1/ping')
+            ->seeStatusCode(200)
+            ->seeJson(['SiteLogger ping successfully']);
+    }
+
+    /**
+     * Ping SiteLogger Microservice DB Test
+     *
+     * @return void
+     */
+    public function testPingDB(): void
+    {
+        $this->get('/api/v1/pingdb')
+            ->seeStatusCode(200)
+            ->seeJson(['SiteLogger PingDB successfully. Database: ' . DB::connection()->getDatabaseName()]);
+    }
+}
